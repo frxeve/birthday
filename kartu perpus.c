c10492129bfc3fe41d042b89711bdfac24859b6d
frxeve <febrianauchiha72@gmail.com>
2024-09-26 22:33:59 +0700

    birthday

---
+++ kartu perpus.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+char Nama[30], NPM [30], Kelas[30], Jurusan[30], Alamat[30], No_HP[30], Email[30];
+
+printf("\n ================================");
+printf("\n Registrasi Anggota Perpustakaan ");
+printf("\n ================================");
+
+printf("\n Nama : ");
+scanf("%s",Nama);
+
+printf("\n NPM : ");
+scanf("%s",NPM); 
+
+
+printf("\n Kelas : ");
+scanf("%s",Kelas);
+
+
+printf("\n Jurusan : ");
+scanf("%s",Jurusan);
+
+
+printf("\n Alamat : ");
+scanf("%s",Alamat);
+
+printf("\n No.HP : ");
+scanf("%s",No_HP);
+
+printf("\n E-mail : ");
+scanf("%s",Email);
+
+printf("\n ============================");
+printf("\n           SELESAI           ");
+printf("\n ============================");
+}
